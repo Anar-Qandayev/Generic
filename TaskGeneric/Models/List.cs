ba23138b070a14cb66c04e18d19d68809db5c529
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-03-30 13:19:50 +0400

    commit

---
+++ TaskGeneric/Models/List.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskGeneric.Models
+{
+    class CustomList<T>
+    {
+        private T[] _list;
+        public CustomList()
+        {
+            _list = new T[0];
+        }
+        public CustomList(params T[] nums)
+        {
+            _list = new T[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                _list[i] = nums[i];
+            }
+        }
+        public void Add(T input)
+        {
+            Array.Resize(ref _list, _list.Length + 1);
+            _list[_list.Length - 1] = input;
+        }
+        public void Show()
+        {
+            foreach (var item in _list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    class Person 
+    {
+        
+        public  int Id { get; set; }
+        public Person()
+        { 
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Id: {Id}");
+        }
+    }
+    class Student : Person 
+    {
+       
+
+        public string Name { get; set; }
+        public void Show()
+        {
+            Console.WriteLine($"Name: {Name}");
+        }
+    }
+}
---
+++ TaskGeneric/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskGeneric.Models;
+
+namespace TaskGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.BackgroundColor
+             = ConsoleColor.Green;
+            Person intList = new Person();
+            Person intList1 = new Person();
+            intList.Id = 345;
+            intList1.Id = 123;
+            intList.Show();
+            intList1.Show();
+            Student stringList = new Student();
+            Student stringList1 = new Student();
+            stringList.Name ="Anar";
+            stringList1.Name = "Atar";
+            stringList.Show();
+            stringList1.Show();
+            Console.WriteLine("----------------------");
+            Console.BackgroundColor
+             = ConsoleColor.Blue; 
+
+            List<int> numbers = new List<int>();
+            numbers.Add(5);
+            numbers.Add(50);
+            numbers.Add(56);
+            numbers.Add(7);
+            numbers.Add(500);    
+            numbers.Add(4008);
+            numbers.Add(8);
+            numbers.Add(58);
+            numbers.LastIndexOf(2);
+            numbers.Remove(50);
+            numbers.Exists(numbers => numbers == 500);
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+            //numbers.Clear();
+            Console.WriteLine(numbers[4]);
+            Console.WriteLine($"Count: {numbers.Count}");
+            Console.BackgroundColor
+             = ConsoleColor.Black;
+
+        }
+    }
+}
